79d66b14f2aac5a02200d22aaa12c38c62c93852
Benefit <101987140+Benefit58@users.noreply.github.com>
2023-11-15 19:57:00 +0800

    Update turboacc.sh

--- turboacc.sh
+++ turboacc.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 
-mkdir -p turboacc_tmp ./package/turboacc
+mkdir -p turboacc_tmp package/turboacc
 
 cd turboacc_tmp 
 git clone https://github.com/chenmozhijin/turboacc -b package
+
 cd ../package/turboacc
+
 git clone https://github.com/fullcone-nat-nftables/nft-fullcone
 git clone https://github.com/chenmozhijin/turboacc
 
@@ -14,9 +16,9 @@ rm -rf ./turboacc
 
 cd ../..
 
-cp -f turboacc_tmp/turboacc/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch
-cp -f turboacc_tmp/turboacc/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
-cp -f turboacc_tmp/turboacc/pending-6.1/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch
+cp -f turboacc_tmp/turboacc/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/meson/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch
+cp -f turboacc_tmp/turboacc/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/meson/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+cp -f turboacc_tmp/turboacc/pending-6.1/613-netfilter_optional_tcp_window_check.patch ./target/linux/meson/pending-6.1/613-netfilter_optional_tcp_window_check.patch
 
 rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
 
@@ -29,5 +31,5 @@ cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboa
 
 rm -rf turboacc_tmp
 
-echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-6.1
-echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-6.1
+echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/meson/config-6.1
+echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/meson/config-6.1
