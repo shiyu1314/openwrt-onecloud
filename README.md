5eb7d3eacadfba05567f1ee5d7d2627d77bf115d
shiyu1314 <shiyu1052@gmail.com>
2026-05-05 14:45:41 +0800

    25.12

---
+++ README.md
@@ -0,0 +1,13 @@
+# openwrt-onecloud-玩客云
+
+## 📋 支持的设备
+- ✅ **玩客云S1608**
+
+- 刷机工具
+  - [下载](https://xd1314.lanzoul.com/iXHbz17bqjhc)
+
+
+- 相关引导
+
+  - [`u-boot`](https://github.com/hzyitc/u-boot-onecloud)
+
---
+++ sh/op.sh
@@ -0,0 +1,157 @@
+#!/bin/bash
+
+set -x
+
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../
+  cd .. && rm -rf $repodir
+}
+
+
+
+git_sparse_clone master https://github.com/immortalwrt/immortalwrt package/emortal/automount
+
+git_sparse_clone master https://github.com/immortalwrt/immortalwrt package/emortal/autosamba
+
+cp -rf {automount,autosamba} package
+
+
+git clone https://github.com/sbwml/autocore-arm package/autocore-arm -b openwrt-25.12 --depth 1
+
+rm -rf package/autocore-arm/.git
+
+git clone -b packages --depth 1 --single-branch https://github.com/shiyu1314/openwrt-feeds package/xd
+git clone -b porxy --depth 1 --single-branch https://github.com/shiyu1314/openwrt-feeds package/porxy
+
+
+rm -rf feeds/luci/applications/{luci-app-dockerman,luci-app-samba4,luci-app-aria2}
+rm -rf feeds/packages/net/{samba4,v2ray-geodata,mosdns,sing-box,aria2,ariang,adguardhome}
+rm -rf feeds/luci/modules/luci-mod-status/htdocs/luci-static/resources/view/status/include/29_ports.js
+
+
+# kenrel Vermagic
+sed -ie 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
+grep HASH target/linux/generic/kernel-6.12 | awk -F'HASH-' '{print $2}' | awk '{print $1}' | md5sum | awk '{print $1}' > .vermagic
+
+
+sed -i 's/^PKG_BUILD_PARALLEL:=1$/PKG_BUILD_PARALLEL:=1\nPKG_FORTIFY_SOURCE:=0/' package/libs/xcrypt/libxcrypt/Makefile
+
+
+
+# drop attendedsysupgrade
+sed -i '/luci-app-attendedsysupgrade/d' \
+    feeds/luci/collections/luci-nginx/Makefile \
+    feeds/luci/collections/luci-ssl-openssl/Makefile \
+    feeds/luci/collections/luci-ssl/Makefile \
+    feeds/luci/collections/luci/Makefile
+    
+sed -i 's/+uhttpd /+luci-nginx /g' feeds/luci/collections/luci/Makefile
+sed -i 's/+uhttpd-mod-ubus //' feeds/luci/collections/luci/Makefile
+sed -i 's/+uhttpd /+luci-nginx /g' feeds/luci/collections/luci-light/Makefile
+sed -i "s/+luci /+luci-nginx /g" feeds/luci/collections/luci-ssl-openssl/Makefile
+sed -i "s/+luci /+luci-nginx /g" feeds/luci/collections/luci-ssl/Makefile
+sed -i 's/+uhttpd +uhttpd-mod-ubus /+luci-nginx /g' feeds/packages/net/wg-installer/Makefile
+sed -i '/uhttpd-mod-ubus/d' feeds/luci/collections/luci-light/Makefile
+sed -i 's/+luci-nginx \\$/+luci-nginx/' feeds/luci/collections/luci-light/Makefile
+
+
+sed -i 's/libustream-mbedtls/libustream-openssl/' include/target.mk
+
+
+
+pushd feeds/luci
+    patch -p1 < 0001-luci-mod-system-add-modal-overlay-dialog-to-reboot.patch
+    patch -p1 < 0002-luci-mod-status-displays-actual-process-memory-usage.patch
+    patch -p1 < 0003-luci-mod-status-storage-index-applicable-only-to-val.patch
+    patch -p1 < 0004-luci-mod-status-firewall-disable-legacy-firewall-rul.patch
+    patch -p1 < 0005-luci-mod-system-add-refresh-interval-setting.patch
+    patch -p1 < 0006-luci-mod-system-mounts-add-docker-directory-mount-po.patch
+    patch -p1 < 0007-luci-mod-system-add-ucitrack-luci-mod-system-zram.js.patch
+    patch -p1 < 0004-luci-add-firewall-add-custom-nft-rule-support.patch
+    patch -p1 < 0008-luci-app-package-manager-support-installing-uploaded.patch
+popd
+
+
+
+patch -p1 --no-backup-if-mismatch < 100-openwrt-firewall4-add-custom-nft-command-support.patch
+patch -p1 --no-backup-if-mismatch < 0012-include-kernel-Always-collect-module-symvers.patch
+patch -p1 --no-backup-if-mismatch < 0013-include-netfilter-update-kernel-config-options-for-l.patch
+patch -p1 --no-backup-if-mismatch < 001-rust-disable-ci-mode.patch
+
+# fstools
+rm -rf package/system/fstools
+git clone https://github.com/sbwml/package_system_fstools -b openwrt-25.12 package/system/fstools
+# util-linux
+rm -rf package/utils/util-linux
+git clone https://github.com/sbwml/package_utils_util-linux -b openwrt-25.12 package/utils/util-linux
+
+# nghttp3
+rm -rf feeds/packages/libs/nghttp3
+git clone https://github.com/sbwml/package_libs_nghttp3 package/libs/nghttp3
+
+# ngtcp2
+rm -rf feeds/packages/libs/ngtcp2
+git clone https://github.com/sbwml/package_libs_ngtcp2 package/libs/ngtcp2
+
+# curl - fix passwall `time_pretransfer` check
+rm -rf feeds/packages/net/curl
+git clone https://github.com/sbwml/feeds_packages_net_curl feeds/packages/net/curl
+
+# nginx - latest version
+rm -rf feeds/packages/net/nginx
+git clone https://github.com/sbwml/feeds_packages_net_nginx feeds/packages/net/nginx -b openwrt-25.12
+sed -i 's/procd_set_param stdout 1/procd_set_param stdout 0/g;s/procd_set_param stderr 1/procd_set_param stderr 0/g' feeds/packages/net/nginx/files/nginx.init
+
+# nginx - ubus
+sed -i 's/ubus_parallel_req 2/ubus_parallel_req 6/g' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+sed -i '/ubus_parallel_req/a\        ubus_script_timeout 300;' feeds/packages/net/nginx/files-luci-support/60_nginx-luci-support
+
+# uwsgi - fix timeout
+sed -i '$a cgi-timeout = 600' feeds/packages/net/uwsgi/files-luci-support/luci-*.ini
+sed -i '/limit-as/c\limit-as = 5000' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+# disable error log
+sed -i "s/procd_set_param stderr 1/procd_set_param stderr 0/g" feeds/packages/net/uwsgi/files/uwsgi.init
+
+# uwsgi - performance
+sed -i 's/threads = 1/threads = 2/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+sed -i 's/processes = 3/processes = 4/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+sed -i 's/cheaper = 1/cheaper = 2/g' feeds/packages/net/uwsgi/files-luci-support/luci-webui.ini
+
+# rpcd - fix timeout
+sed -i 's/option timeout 30/option timeout 60/g' package/system/rpcd/files/rpcd.config
+sed -i 's#20) \* 1000#60) \* 1000#g' feeds/luci/modules/luci-base/htdocs/luci-static/resources/rpc.js
+
+# luci-compat - remove extra line breaks from description
+sed -i '/<br \/>/d' feeds/luci/modules/luci-compat/luasrc/view/cbi/full_valuefooter.htm
+
+
+#golang 26.x
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+
+
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
+
+sudo rm -rf package/base-files/files/etc/banner
+
+sed -i "s/%D %V %C/%D %V $(TZ=UTC-8 date +%Y.%m.%d)/" package/base-files/files/etc/openwrt_release
+
+sed -i "s/%R/by $OP_author/" package/base-files/files/etc/openwrt_release
+
+date=$(date +"%Y-%m-%d")
+echo "                                                    " >> package/base-files/files/etc/banner
+echo "  _______                     ________        __" >> package/base-files/files/etc/banner
+echo " |       |.-----.-----.-----.|  |  |  |.----.|  |_" >> package/base-files/files/etc/banner
+echo " |   -   ||  _  |  -__|     ||  |  |  ||   _||   _|" >> package/base-files/files/etc/banner
+echo " |_______||   __|_____|__|__||________||__|  |____|" >> package/base-files/files/etc/banner
+echo "          |__|" >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
+echo "         %D ${date} by $OP_author                     " >> package/base-files/files/etc/banner
+echo " -----------------------------------------------------" >> package/base-files/files/etc/banner
